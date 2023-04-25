097bc5617de724bf56c52d7da6fc19137f74c446
MinhTriet2104 <minhtriet2104.it@gmail.com>
2023-04-25 18:50:20 +0700

    add highlight syntax

--- components/ShowdownWrapper.tsx
+++ components/ShowdownWrapper.tsx
@@ -1,9 +1,18 @@
 import showdown from 'showdown';
+import showdownHighlight from 'showdown-highlight';
+import 'highlight.js/styles/base16/dracula.css';
 
 // Tạo một instance của showdown
-const converter = new showdown.Converter();
+const converter = new showdown.Converter({
+  extensions: [showdownHighlight({
+    // Whether to add the classes to the <pre> tag, default is false
+    pre: true,
+    // Whether to use hljs' auto language detection, default is true
+    auto_detection: true
+  })]
+});
 
 // Hàm chuyển đổi Markdown sang HTML
-export const markdownToHtml = (markdown: string):string => {
+export const markdownToHtml = (markdown: string): string => {
   return converter.makeHtml(markdown);
 };
